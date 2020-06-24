d5700e9aaf70e6b37329143f184f3bf5b95a39e9
Jc7j <chiangjason19@gmail.com>
2020-06-23 22:08:59 -0700

    Added further styling to Header and created NumberDisplay component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
-import "./App.scss";
+import NumberDisplay from '../NumberDisplay';
+
+import './App.scss';
 
 const App: React.FC = () => {
-     
-    return (
-        <div className="App">
-            <div className="Header">
-                Header
-            </div>
-            <div className="Body">
-                Body
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="App">
+      <div className="Header">
+        <NumberDisplay value={0} />
+        <span className="Face" role="img" aria-label="face">
+          🙂{' '}
+        </span>
+        <NumberDisplay value={23} />
+      </div>
+      <div className="Body">Body</div>
+    </div>
+  );
+};
 
 export default App;
